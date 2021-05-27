cfeb5d7031c5b3b73c8ba1a7f98e1015d72a4787
Roman Podstenny <romanoidprg@mail.ru>
2021-05-27 23:02:17 +0300

    delete

--- Stage1/Build tools/utils/src/main/java/com/epam/mjc/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.epam.mjc;
-
-import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
-
-public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        return isCreatable(str) && str.charAt(0) != '-';
-    }
-}
