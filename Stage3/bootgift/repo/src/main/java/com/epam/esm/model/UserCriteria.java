15a6e213193d507d085d2969c43dba5817b456fd
Raman Padstsenny <romanoidprg@mail.ru>
2021-06-29 15:38:51 +0300

    made user order entity

---
+++ Stage3/bootgift/repo/src/main/java/com/epam/esm/model/UserCriteria.java
@@ -0,0 +1,64 @@
+package com.epam.esm.model;
+
+public class UserCriteria {
+    private final String tagName;
+    private final String name;
+    private final String description;
+    private final boolean sortByName;
+    private final boolean sortByCrDate;
+    private final boolean sortByUpdDate;
+    private final String sortNameOrder;
+    private final String sortCrDateOrder;
+    private final String sortUpdDateOrder;
+
+
+    public UserCriteria(String tagName, String name, String description,
+                        boolean sortByName, boolean sortByCrDate, boolean sortByUpdDate,
+                        String sortNameOrder, String sortCrDateOrder, String sortUpdDateOrder) {
+        this.tagName = tagName != null ? tagName : "";
+        this.name = name != null ? name : "";
+        this.description = description != null ? description : "";
+        this.sortByName = sortByName;
+        this.sortByCrDate = sortByCrDate;
+        this.sortByUpdDate = sortByUpdDate;
+        this.sortNameOrder = "asc".equalsIgnoreCase(sortNameOrder) || "desc".equalsIgnoreCase(sortNameOrder) ? sortNameOrder : "";
+        this.sortCrDateOrder = "asc".equalsIgnoreCase(sortCrDateOrder) || "desc".equalsIgnoreCase(sortCrDateOrder) ? sortCrDateOrder : "";
+        this.sortUpdDateOrder = "asc".equalsIgnoreCase(sortUpdDateOrder) || "desc".equalsIgnoreCase(sortUpdDateOrder) ? sortUpdDateOrder : "";
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isSortByName() {
+        return sortByName;
+    }
+
+    public boolean isSortByCrDate() {
+        return sortByCrDate;
+    }
+
+    public boolean isSortByUpdDate() {
+        return sortByUpdDate;
+    }
+
+    public String getSortNameOrder() {
+        return sortNameOrder;
+    }
+
+    public String getSortCrDateOrder() {
+        return sortCrDateOrder;
+    }
+
+    public String getSortUpdDateOrder() {
+        return sortUpdDateOrder;
+    }
+}
